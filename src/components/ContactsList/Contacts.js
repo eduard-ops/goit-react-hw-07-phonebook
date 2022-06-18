d45a7901fc6e2cbbf019f1fcd230003a5844fe55
eduard-ops <epustynnik@gmail.com>
2022-06-18 15:31:42 +0300

    edit Contacts.js

--- src/components/ContactsList/Contacts.js
+++ src/components/ContactsList/Contacts.js
@@ -2,29 +2,29 @@ import s from './Contacts.module.css';
 
 import PropTypes from 'prop-types';
 
-import { useInfo } from 'redux/contacts/contactsReducer';
+import ContactItem from 'components/ContactItem';
 
-export default function Contacts() {
-  const { remove, show } = useInfo();
-  const items = show();
+import { useGetContactsQuery } from 'redux/contacts/contactsRtkSlice';
+
+export default function Contacts({ item }) {
+  const { data, error, isLoading } = useGetContactsQuery();
 
   return (
     <ul className={s.list}>
-      {items.map(({ id, name, number }) => {
-        return (
-          <li key={id} className={s.item}>
-            <p>{name}:</p>
-            <span className={s.tel}>{number}</span>
-            <button
-              type="button"
-              className={s.button}
-              onClick={() => remove(id)}
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
+      {isLoading ? (
+        <b>Загружаем контакты</b>
+      ) : (
+        item.map(item => {
+          return (
+            <ContactItem
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              phone={item.phone}
+            />
+          );
+        })
+      )}
     </ul>
   );
 }
