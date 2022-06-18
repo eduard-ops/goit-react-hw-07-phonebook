bf52028cef915366546898b83194de1d1a7a4cd0
eduard-ops <epustynnik@gmail.com>
2022-06-18 15:31:12 +0300

    Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,14 +2,11 @@ import s from './Filter.module.css';
 
 import PropTypes from 'prop-types';
 
-import { useInfo } from 'redux/contacts/contactsReducer';
-
-export default function Filter() {
-  const { change } = useInfo();
+export default function Filter({ value, onChange }) {
   return (
     <label className={s.label}>
       Find Contacts by name
-      <input onChange={e => change(e.target.value)} />
+      <input value={value} onChange={e => onChange(e.target.value)} />
     </label>
   );
 }
